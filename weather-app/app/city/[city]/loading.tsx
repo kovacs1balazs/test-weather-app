548de8fc9aa9dc0e8ff8ec88620ad9c80c1d078f
Kbalazsarteries <kovacs.balazs@arteries.hu>
2025-08-28 17:35:05 +0200

    feat: loading page

---
+++ weather-app/app/city/[city]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <main className="p-8 flex justify-center items-center h-screen bg-gradient-to-b from-sky-100 to-blue-200">
+      <p className="text-2xl animate-pulse">Betöltés...</p>
+    </main>
+  );
+}
